86b8e7efe1a1039d4eb2fa006740fdd27c14f9a1
Matt Ludwigs <mattludwigs@gmail.com>
2019-12-12 20:30:06 -0700

    Add close and get line info

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ NIF = ./priv/cdev_nif.so
 
 all:
 	mkdir -p priv
-	gcc -o $(NIF) -I/usr/lib/erlang/erts-10.4/include -fpic -shared src/cdev_nif.c
+	gcc -o $(NIF) -I$(ERL_INCLUDE_PATH) -fpic -shared src/cdev_nif.c
 
 clean:
 	rm -r priv
--- lib/circuits_cdev.ex
+++ lib/circuits_cdev.ex
@@ -13,7 +13,15 @@ defmodule Circuits.GPIO.Chip do
       :erlang.nif_error(:nif_not_loaded)
     end
 
+    def close(_chip) do
+      :erlang.nif_error(:nif_not_loaded)
+    end
+
     def get_info(_chip_ref) do
+      :erlang.nif_error(:nif_not_loaded)
+    end
+
+    def get_line_info(_chip, _line_offset) do
       :erlang.nif_error(:nif_not_loaded)
     end
   end
@@ -25,10 +33,30 @@ defmodule Circuits.GPIO.Chip do
     |> Nif.open()
   end
 
+  @spec close(reference()) :: :ok
+  def close(chip), do: Nif.close(chip)
+
   @spec get_info(reference()) :: %{name: String.t(), label: String.t(), lines: integer()}
   def get_info(chip) do
     {name, label, lines} = Nif.get_info(chip)
 
     %{name: to_charlist(name), label: to_charlist(label), lines: lines}
   end
+
+  def get_line_info(chip, line_offset) do
+    {flags, name, consumer} = Nif.get_line_info(chip, line_offset)
+
+    # TODO: unmask flags, move to proper data structure
+    %{
+      flags: flags,
+      name: line_name_to_string_or_nil(name),
+      consumer: line_consumer_to_string_or_nil(consumer)
+    }
+  end
+
+  defp line_name_to_string_or_nil([]), do: nil
+  defp line_name_to_string_or_nil(line_name), do: to_string(line_name)
+
+  defp line_consumer_to_string_or_nil([]), do: nil
+  defp line_consumer_to_string_or_nil(consumer), do: to_string(consumer)
 end

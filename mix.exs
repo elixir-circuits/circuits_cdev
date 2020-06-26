3a7280a85a73998b1d7bffadad6438d346d740f6
Frank Hunleth <fhunleth@troodon-software.com>
2020-06-26 15:40:52 -0400

    Add base mix tasks and functions

--- mix.exs
+++ mix.exs
@@ -1,21 +1,32 @@
-defmodule CircuitsCdev.MixProject do
+defmodule Circuits.Cdev.MixProject do
   use Mix.Project
+
+  @version "0.1.0"
+  @source_url "https://github.com/elixir-circuits/circuits_cdev"
 
   def project do
     [
       app: :circuits_cdev,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.9",
-      compilers: [:elixir_make] ++ Mix.compilers(),
+      description: description(),
+      package: package(),
+      source_url: @source_url,
+      compilers: [:elixir_make | Mix.compilers()],
       make_targets: ["all"],
       make_clean: ["clean"],
+      docs: docs(),
+      aliases: [format: [&format_c/1, "format"]],
       start_permanent: Mix.env() == :prod,
       build_embedded: true,
-      deps: deps()
+      dialyzer: [
+        flags: [:unmatched_returns, :error_handling, :race_conditions, :underspecs]
+      ],
+      deps: deps(),
+      preferred_cli_env: %{docs: :docs, "hex.publish": :docs, "hex.build": :docs}
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -23,11 +34,52 @@ defmodule CircuitsCdev.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp description do
+    "Use GPIOs in Elixir"
+  end
+
+  defp package do
+    %{
+      files: [
+        "lib",
+        "src/*.[ch]",
+        "mix.exs",
+        "README.md",
+        "LICENSE",
+        "CHANGELOG.md",
+        "Makefile"
+      ],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    }
+  end
+
   defp deps do
     [
       {:elixir_make, "~> 0.6", runtime: false},
-      {:ex_doc, "~> 0.21.3", only: :docs, runtime: false}
+      {:ex_doc, "~> 0.21.3", only: :docs, runtime: false},
+      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false}
     ]
   end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url
+    ]
+  end
+
+  defp format_c([]) do
+    case System.find_executable("astyle") do
+      nil ->
+        Mix.Shell.IO.info("Install astyle to format C code.")
+
+      astyle ->
+        System.cmd(astyle, ["-n", "src/*.c"], into: IO.stream(:stdio, :line))
+    end
+  end
+
+  defp format_c(_args), do: true
 end

2dc2f062ce8ce6dec0d15d88602de390f738382b
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2021-03-18 20:39:39 -0700

    Add example project

---
+++ example/lib/example/application.ex
@@ -0,0 +1,43 @@
+defmodule Example.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Example.Supervisor]
+
+    children =
+      [
+        # Children for all targets
+        # Starts a worker by calling: Example.Worker.start_link(arg)
+        # {Example.Worker, arg},
+      ] ++ children(target())
+
+    Supervisor.start_link(children, opts)
+  end
+
+  # List all child processes to be supervised
+  def children(:host) do
+    [
+      # Children that only run on the host
+      # Starts a worker by calling: Example.Worker.start_link(arg)
+      # {Example.Worker, arg},
+    ]
+  end
+
+  def children(_target) do
+    example_args = Application.get_env(:example, :config, [])
+
+    [
+      {Example, example_args}
+    ]
+  end
+
+  def target() do
+    Application.get_env(:example, :target)
+  end
+end

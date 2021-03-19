8287ca1b99fa4bd72f84ab1ac01ffbd745bb7489
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2021-03-18 20:50:20 -0700

    Add CI and make CI work

--- test/circuits_cdev_test.exs
+++ test/circuits_cdev_test.exs
@@ -1,8 +1,2 @@
-defmodule CircuitsCdevTest do
-  use ExUnit.Case
-  doctest CircuitsCdev
-
-  test "greets the world" do
-    assert CircuitsCdev.hello() == :world
-  end
+defmodule Circuits.GPIO.Chip.Test do
 end
